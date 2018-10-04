5cddf377c57e30b22f3c9f23dfea04c04cfe050e
Kpres <kevinfpresing@gmail.com>
2018-10-04 10:26:33 -0400

    Initialized Home Page

---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './WelcomePage.css';
+
+export class HomePage extends Component{
+    constructor(){
+
+    }
+
+    render(){
+        return(<div>This is Home Page</div>);
+    }
+}
