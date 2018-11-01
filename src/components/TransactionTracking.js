516a713bd54a679e09cf44d2eaa360801530338a
argentinoj <argentinoj.0.2.0.4@gmail.com>
2018-11-01 15:17:10 -0400

    transaction tracking framework

--- src/components/TransactionTracking.js
+++ src/components/TransactionTracking.js
@@ -1,2 +1,36 @@
 import React, {Component} from 'react';
 import './TransactionTracking.css';
+
+export class TransactionTracking extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            
+        }
+
+        this.addTransaction = this.addTransaction.bind(this);
+    }
+
+    addTransaction(name, value ){
+
+    }
+
+
+
+};
+
+export class TransactionItem extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            
+        }
+
+    }
+
+
+
+
+};
