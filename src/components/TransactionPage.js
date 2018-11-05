735f7abfa18282c32b36506ce521b6a5506bd3ab
Kevin Zhan <kevin.w.zhan@gmail.com>
2018-11-04 21:23:39 -0500

    add page structure & background

--- src/components/TransactionPage.js
+++ src/components/TransactionPage.js
@@ -4,14 +4,29 @@ import './TransactionPage.css';
 export class TransactionPage extends Component{
     constructor(){
         super();
-
+        this.state = ({
+            value: 0,
+            positive = false,
+            name: "",
+            spontaneous: true
+        });
     }
 
     render(){
         return(
-            <div>
-                <h1>Hello World</h1>
+            <div className = "page">
+                <div className = "header">
+                ∇
+                <br/>
+                ${this.state.name}
+                </div>
+                <div className = "transaction">
+
+                </div>
+                <div className = "regularity">
+
+                </div>
             </div>
-        )
+        );
     }
 }
