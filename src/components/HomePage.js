b09227822f25a6963cc4e8d0b50c7116f65a350b
Pheonix <colecarpenter@gmail.com>
2018-11-05 11:36:17 -0500

    completed spendable amount implementation with placeholder values for now

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
+import {ManagementPage} from './ManagementPage.js'
 import './HomePage.css';
 
 export class HomePage extends Component{
     constructor(){
         super();
+        this.current_savings_percent = 20,
+        this.total_wallet_amount = 15000,
         this.state = ({
-            wallet: 25000,
+            wallet: ((100-this.current_savings_percent)/100 * this.total_wallet_amount),
             wallet_sign: "+",
         });
     }
