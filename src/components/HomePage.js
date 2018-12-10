c989f4d4141dc117a358179f6cbd9c473caace13
Kpres <kevinfpresing@gmail.com>
2018-12-10 14:54:30 -0500

    forced two decimal places on spendable amount

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -27,7 +27,7 @@ export class HomePage extends Component{
     //updates the total wallet or spendable amount to be shown
     updateWallet(){
         this.setState(
-            {wallet: Math.round(100 * (100 - this.props.current_savings_percent) / 100 * this.props.total_wallet_amount) / 100}
+            { wallet: parseFloat((100 * (100 - this.props.current_savings_percent) / 100 * this.props.total_wallet_amount) / 100).toFixed(2) }
         )
     }
 
